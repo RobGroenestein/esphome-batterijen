2b9387417fb4f5a1e825feacb1c11d522e603924
RobGroenestein <rob@groenestein.eu>
2025-08-13 19:23:58 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,31 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import uart, sensor
+from esphome.const import CONF_ID
+
+DEPENDENCIES = ["uart"]
+
+batterijen_ns = cg.esphome_ns.namespace("batterijen")
+Batterijen = batterijen_ns.class_("Batterijen", cg.Component, uart.UARTDevice)
+
+CONF_PACK_ID = "pack_id"
+CONF_CELLS = "cells"
+
+CONFIG_SCHEMA = (
+    cv.Schema({
+        cv.GenerateID(): cv.declare_id(Batterijen),
+        cv.Required(CONF_PACK_ID): cv.int_,
+        cv.Required(CONF_CELLS): cv.ensure_list(sensor.sensor_schema()),
+    })
+    .extend(uart.UART_DEVICE_SCHEMA)
+)
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await uart.register_uart_device(var, config)
+    cg.add(var.set_pack_id(config[CONF_PACK_ID]))
+
+    for i, sens_conf in enumerate(config[CONF_CELLS]):
+        sens = await sensor.new_sensor(sens_conf)
+        cg.add(var.set_cell_sensor(i, sens))
