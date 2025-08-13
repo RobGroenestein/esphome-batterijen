d4afb20d23ad12ac2a64f9b7ff1da979f1c9d669
RobGroenestein <rob@groenestein.eu>
2025-08-13 18:46:54 +0200

    Create batterijen.cpp

---
+++ batterijen/components/batterijen.cpp
@@ -0,0 +1,10 @@
+#include "batterijen.h"
+#include "esphome/core/log.h"
+
+namespace esphome {
+namespace batterijen {
+
+static const char *TAG = "batterijen";
+
+}  // namespace batterijen
+}  // namespace esphome
